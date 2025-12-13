9efc2b08d3472c177148de810750cfd04674c0ca
Shreeram2k3 <kanishreeram@gmail.com>
2025-12-13 10:07:50 +0530

    OOP basics like Classes,Objects, inheritance, polymorphism, encapsulation covered

--- OOP_Basics/Main.java
+++ OOP_Basics/Main.java
@@ -1,12 +1,28 @@
 package OOP_Basics;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
        Student std1 = new Student(107,"Shreeram",95.5);
-       std1.setPass("1234");
 
-        System.out.println(std1);
+        std1.setPass("1234");
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println(std1.getPass());
+        String upass =inp.next();
+
+        if(upass.equals(std1.getPass()))
+        {
+            System.out.println("welcome user");
+        }
+        else
+        {
+            System.out.println("unauthorized");
+        }
+
 
 
 
